d11da1a6147a846194ec0a416071b855e867df60
Hoormazd Pirayeshfar <hoormazd@pirayeshfar.ch>
2023-09-09 23:47:15 +0200

    Drugs.com scraper done

--- Scrapy/medication_scraper/medication_scraper/spiders/medications.py
+++ Scrapy/medication_scraper/medication_scraper/spiders/medications.py
@@ -7,6 +7,32 @@ class MedicationsSpider(scrapy.Spider):
     start_urls = [
         "https://www.drugs.com/alpha/a.html",
         "https://www.drugs.com/alpha/b.html",
+        "https://www.drugs.com/alpha/c.html",
+        "https://www.drugs.com/alpha/d.html",
+        "https://www.drugs.com/alpha/e.html",
+        "https://www.drugs.com/alpha/f.html",
+        "https://www.drugs.com/alpha/g.html",
+        "https://www.drugs.com/alpha/h.html",
+        "https://www.drugs.com/alpha/i.html",
+        "https://www.drugs.com/alpha/j.html",
+        "https://www.drugs.com/alpha/k.html",
+        "https://www.drugs.com/alpha/l.html",
+        "https://www.drugs.com/alpha/m.html",
+        "https://www.drugs.com/alpha/n.html",
+        "https://www.drugs.com/alpha/o.html",
+        "https://www.drugs.com/alpha/p.html",
+        "https://www.drugs.com/alpha/q.html",
+        "https://www.drugs.com/alpha/r.html",
+        "https://www.drugs.com/alpha/s.html",
+        "https://www.drugs.com/alpha/t.html",
+        "https://www.drugs.com/alpha/u.html",
+        "https://www.drugs.com/alpha/v.html",
+        "https://www.drugs.com/alpha/w.html",
+        "https://www.drugs.com/alpha/x.html",
+        "https://www.drugs.com/alpha/y.html",
+        "https://www.drugs.com/alpha/z.html",
+        "https://www.drugs.com/alpha/0-9.html",
+
         # Add more URLs for other letters if needed
     ]
 
@@ -16,6 +42,7 @@ class MedicationsSpider(scrapy.Spider):
         
         for url in medication_urls:
             yield response.follow(url, callback=self.parse_page)
+            yield response.follow(url, callback=self.parse_medication)
     
     def parse_page(self, response):
         medication_urls = response.css('#content .ddc-list-unstyled a::attr(href)').extract()
@@ -29,11 +56,24 @@ class MedicationsSpider(scrapy.Spider):
         # Populate item fields
         item['source'] = "drugs.com"
         item['source_url'] = response.url
-        item['name'] = response.xpath('//h1/text()').get()
-        item['generic_name'] = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "drug-subtitle", " " ))]/text()').get()
-        item['description'] = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "drug-subtitle", " " ))]/text()').get()
-        item['warnings'] = response.xpath('//*[@id="s-34071-1"]//*[contains(concat( " ", @class, " " ), concat( " ", "First", " " ))]/text()').get()
-        item['side_effects'] = response.xpath('//*[@id="s-34084-4"]//*[contains(concat( " ", @class, " " ), concat( " ", "First", " " ))]/text()').get()
-        item['how_to_take'] = response.xpath('//*[@id="s-34068-7"]//p/text()').getall()
+        item['name'] = response.css('h1::text').get()
+        item['generic_name'] = response.css('.drug-subtitle').get()
 
-        yield item
+        item['description'] = response.css('#uses + p').get()
+        if (item['description'] == ''):
+            item['description'] = response.css('#s-34089-3 .First').get()
+
+        item['side_effects'] = response.css('#side-effects+ p').get()
+        if (item['side_effects'] == ''):
+            item['side_effects'] = response.css('#s-34084-4 .First').get()
+
+        item['warnings'] = response.css('#warnings+ p').getall()
+        if (item['warnings'] == ''):
+            item['warnings'] = response.css('#s-34071-1 .First').getall()
+
+        item['how_to_take'] = response.css('#dosage+ p').getall()
+        if (item['how_to_take'] == ''):
+            item['how_to_take'] = response.css('#s-34067-9 .First').getall()
+
+        if (item['name'] != ''):
+            yield item
